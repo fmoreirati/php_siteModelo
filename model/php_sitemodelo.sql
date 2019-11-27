75e86ffa4808e3c63db7f5d5ed52fd30d099794e
Técnico em Informática 2019.2 <39121742019.2@senacrjedu.local>
2019-11-27 14:29:46 -0300

    27112019

--- model/php_sitemodelo.sql
+++ model/php_sitemodelo.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Máquina: localhost
--- Data de Criação: 22-Nov-2019 às 20:02
+-- Data de Criação: 27-Nov-2019 às 17:28
 -- Versão do servidor: 5.6.13
 -- versão do PHP: 5.4.17
 
@@ -11,23 +11,11 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Base de Dados: `phpsitemodelo`
---
-
--- --------------------------------------------------------
-
---
 -- Estrutura da tabela `produto`
 --
 
 CREATE TABLE IF NOT EXISTS `produto` (
-  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) DEFAULT NULL,
   `descricao` varchar(100) DEFAULT NULL,
   `valor` decimal(12,2) NOT NULL,
@@ -37,14 +25,14 @@ CREATE TABLE IF NOT EXISTS `produto` (
   `foto3` varchar(100) NOT NULL,
   `foto4` varchar(100) NOT NULL,
   `foto5` varchar(100) NOT NULL,
-  PRIMARY KEY (`uid`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Extraindo dados da tabela `produto`
 --
 
-INSERT INTO `produto` (`uid`, `nome`, `descricao`, `valor`, `quant`, `foto1`, `foto2`, `foto3`, `foto4`, `foto5`) VALUES
+INSERT INTO `produto` (`id`, `nome`, `descricao`, `valor`, `quant`, `foto1`, `foto2`, `foto3`, `foto4`, `foto5`) VALUES
 (1, 'Downward', 'Jogo medieval', '0.00', 0, '', '', '', '', '');
 
 -- --------------------------------------------------------
@@ -96,6 +84,38 @@ INSERT INTO `usuario` (`uid`, `nome`, `nickname`, `email`, `senha`, `id-tipo`) V
 (3, 'Pedro', 'pp2', 'pedro@email.com', '111111', 1),
 (4, 'Mario', 'marinho', 'mario@email.com', '111111', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `venda`
+--
+
+CREATE TABLE IF NOT EXISTS `venda` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `data` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `uid_usuario` int(11) NOT NULL,
+  `valor` decimal(12,2) NOT NULL,
+  `aberto` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `uid_usuario` (`uid_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `venda_produto`
+--
+
+CREATE TABLE IF NOT EXISTS `venda_produto` (
+  `id_venda` int(11) NOT NULL,
+  `id_produto` int(11) NOT NULL,
+  `quant` int(11) NOT NULL,
+  `valor_produto` decimal(12,2) NOT NULL,
+  `valor_total` decimal(12,2) NOT NULL,
+  KEY `id_venda` (`id_venda`,`id_produto`),
+  KEY `id_produto` (`id_produto`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Constraints for dumped tables
 --
@@ -106,6 +126,17 @@ INSERT INTO `usuario` (`uid`, `nome`, `nickname`, `email`, `senha`, `id-tipo`) V
 ALTER TABLE `usuario`
   ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id-tipo`) REFERENCES `tipo` (`id-tipo`);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Limitadores para a tabela `venda`
+--
+ALTER TABLE `venda`
+  ADD CONSTRAINT `venda_ibfk_1` FOREIGN KEY (`uid_usuario`) REFERENCES `usuario` (`uid`);
+
+--
+-- Limitadores para a tabela `venda_produto`
+--
+ALTER TABLE `venda_produto`
+  ADD CONSTRAINT `venda_produto_ibfk_2` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id`),
+  ADD CONSTRAINT `venda_produto_ibfk_1` FOREIGN KEY (`id_venda`) REFERENCES `venda` (`id`);
+
+
